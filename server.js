d91dee50c4d2205133d16e4909e5ee1ddfda44b3
Nicolas Chambrier <naholyr@gmail.com>
2012-12-10 18:32:43 +0100

    Refactor actions listings

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ var async = require('async');
 
 var gith = require('gith').create(process.env.PORT || 9001);
 
-var tests = require('./lib/tests');
-var actions = require('./lib/actions');
+var allTests = require('./lib/tests');
+var allActions = require('./lib/actions');
+
+function getMethods (config, names, type) {
+  var methods = type === 'test' ? allTests : allActions;
+  var defaultName = type === 'test' ? 'pass' : 'noop';
+  names = names || [];
+  if (!Array.isArray(names)) names = [names];
+  return names.map(function (name) {
+    return (methods[name || defaultName] || function (config, payload, cb) {
+      cb(new Error('Configuration error: unknown ' + type + ' "' + name + '"'));
+    }).bind(null, config);
+  });
+}
 
 
 fs.readdirSync(path.join(__dirname, 'projects'))
@@ -35,61 +47,34 @@ fs.readdirSync(path.join(__dirname, 'projects'))
 
   var projectFilter = config.filter || {};
 
-  var projectTests = config.test || [];
-  if (!Array.isArray(projectTests)) projectTests = [projectTests];
-  projectTests = projectTests.map(function (name) {
-    return (tests[name || 'pass'] || function (config, payload, cb) {
-      cb(new Error('Configuration error: unknown test "' + name + '"'));
-    }).bind(null, config);
-  });
-
-  var projectIfPass = config.pass || [];
-  if (!Array.isArray(projectIfPass)) projectIfPass = [projectIfPass];
-  projectIfPass = projectIfPass.map(function (name) {
-    return (actions[name || 'noop'] || function (config, payload, cb) {
-      cb(new Error('Configuration error: unknown action "' + name + '"'));
-    }).bind(null, 'pass', config);
-  });
-
-  var projectIfFail = config.fail || [];
-  if (!Array.isArray(projectIfFail)) projectIfFail = [projectIfFail];
-  projectIfFail = projectIfFail.map(function (name) {
-    return (actions[name || 'noop'] || function (config, payload, cb) {
-      cb(new Error('Configuration error: unknown action "' + name + '"'));
-    }).bind(null, 'fail', config);
-  });
-
-  var projectIfError = config.fail || [];
-  if (!Array.isArray(projectIfError)) projectIfError = [projectIfError];
-  projectIfError = projectIfError.map(function (name) {
-    return (actions[name || 'noop'] || function (config, payload, cb) {
-      cb(new Error('Configuration error: unknown action "' + name + '"'));
-    }).bind(null, 'error', config);
-  });
-
   var finished = function finished () {
     console.error(project, arguments);
   };
 
-  config.processes.forEach(function (action) {
-    gith(merge(projectFilter, action.filter || {})).on(action.event || 'all', function (payload) {
-      async.series(projectTests.map(function (test) {
+  config.processes.forEach(function (process) {
+    var tests = getMethods(config, config.test, 'test');
+    var passActions = getMethods(config, config.pass, 'action');
+    var failActions = getMethods(config, config.fail, 'action');
+    var errorActions = getMethods(config, config.error, 'action');
+
+    gith().on(process.event || 'all', function (payload) {
+      async.series(tests.map(function (test) {
         return test.bind(null, payload);
       }), function (err, results) {
         var actions;
         if (err) {
-          actions = projectsIfError;
+          actions = errorActions;
         } else {
           var pass = results.some(function (result) {
             return Array.isArray(result) ? !!result[0] : !!result;
           });
-          actions = pass ? projectsIfPass : projectsIfFail;
+          actions = pass ? passActions : failActions;
         }
         async.series(actions.map(function (action) {
           return action.bind(null, payload, err, results);
         }), function (err, results) {
           if (err) {
-            async.series(projectsIfError.map(function (action) {
+            async.series(errorActions.map(function (action) {
               return action.bind(null, payload, err, results);
             }), finished);
           } else {
