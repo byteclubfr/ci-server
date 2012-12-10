717cd3c29feabe7d48bcbe5cd1235788fddc699e
Nicolas Chambrier <naholyr@gmail.com>
2012-12-10 21:25:40 +0100

    Git working directory

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ var gith = require('gith').create(process.env.PORT || 9001);
 var allTests = require('./lib/tests');
 var allActions = require('./lib/actions');
 
-function getMethods (config, names, type) {
+console.debug = console.log;
+
+function getMethods (config, option, type) {
   var methods = type === 'test' ? allTests : allActions;
   var defaultName = type === 'test' ? 'pass' : 'noop';
-  names = names || [];
+  var names = config[option] || [];
   if (!Array.isArray(names)) names = [names];
   return names.map(function (name) {
-    return (methods[name || defaultName] || function (config, payload, cb) {
+    var foo = methods[name || defaultName] || function (config, payload, cb) {
       cb(new Error('Configuration error: unknown ' + type + ' "' + name + '"'));
-    }).bind(null, config);
+    };
+    return type === 'action' ? foo.bind(null, option, config) : foo.bind(null, config);
   });
 }
 
@@ -52,12 +55,14 @@ fs.readdirSync(path.join(__dirname, 'projects'))
   };
 
   config.processes.forEach(function (process) {
-
     process.filter = merge(projectFilter, process.filter || {});
-    var tests = getMethods(process, process.test, 'test');
-    var passActions = getMethods(process, process.pass, 'action');
-    var failActions = getMethods(process, process.fail, 'action');
-    var errorActions = getMethods(process, process.error, 'action');
+    var tests = getMethods(process, 'test', 'test');
+    var passActions = getMethods(process, 'pass', 'action');
+    var failActions = getMethods(process, 'fail', 'action');
+    var errorActions = getMethods(process, 'error', 'action');
+
+    // Prepare working directory before tests
+    tests.unshift(prepareWorkingDirectory.bind(null, project, process));
 
     gith(process.filter).on(process.event || 'all', function (payload) {
       async.series(tests.map(function (test) {
@@ -89,3 +94,65 @@ fs.readdirSync(path.join(__dirname, 'projects'))
 
   console.log('Listening on port', process.env.PORT || 9001);
 });
+
+
+var spawn = require('child_process').spawn;
+
+function prepareWorkingDirectory (project, config, payload, cb) {
+  var dir = path.join(__dirname, 'projects', project, 'git');
+
+  var output = '';
+  var doSpawn = function (command, args, cb) {
+    var cwd = process.cwd();
+    process.chdir(dir);
+    var done = function (err) {
+      process.chdir(cwd);
+      cb(err);
+    };
+    var cmd = spawn(command, args);
+    cmd.stdout.on('data', function (data) { output += 'stdout: ' + data; });
+    cmd.stderr.on('data', function (data) { output += 'stderr: ' + data; });
+    cmd.on('error', function (err) { done(err); });
+    cmd.on('exit', function (code) { done(code ? new Error('Command "' + command + ' \'' + args.join('\' \'') + '\' failed with status ' + code) : null); });
+  };
+
+  var sha = payload.sha || payload.branch; // only in "push" // TODO other events
+
+  if (!sha) {
+    return cb(new Error('Unable to guess commit-ish: will not be able to initialize working directory'));
+  }
+
+  async.waterfall([
+    function statGit (cb) {
+      fs.stat(path.join(dir, '.git'), function (err, stat) {
+        if (err && err.code === 'ENOENT') err = null, stat = null;
+        if (stat && !stat.isDirectory()) err = new Error('Path "' + path.join(dir, '.git') + '" exists and is not a directory!');
+        return cb(err, stat);
+      });
+    },
+    function gitFetch (exists, cb) {
+      if (exists) {
+        output += '>> Git repository: already cloned, fetch remote data…\n';
+        doSpawn('git', ['fetch', 'origin'], cb);
+      } else {
+        output += '>> Git repository: clone remote…\n';
+        var url = payload.urls.repo; // Shoulw be common to all payloads
+        if (!url) {
+          cb(new Error('Could not retrieve repository URL: cannot clone!'));
+        } else {
+          if (url.match(/^https?:\/\//)) {
+            // Prefer the SSH git protocol, using deploy key
+            url = url.replace(/^https?:\/\/(.*?)\//, 'git@$1:') + '.git';
+          }
+          doSpawn('git', ['clone', url, dir], cb);
+        }
+      }
+    },
+    function gitCheckout (cb) {
+      output += '>> Git repository: checkout ' + sha + '\n';
+      doSpawn('git', ['checkout', sha], cb);
+    }
+  ], function (err) {
+    cb(err, output);
+  });
+}
